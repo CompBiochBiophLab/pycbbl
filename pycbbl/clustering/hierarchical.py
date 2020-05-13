254933e4f02e4047030e9dc02f247700a30996af
Martin-Floor <martinfloor@gmail.com>
2020-05-13 10:36:15 +0200

    Clustering method folder incorporated

--- pycbbl/clustering/hierarchical.py
+++ pycbbl/clustering/hierarchical.py
@@ -1,6 +1,33 @@
+import numpy as np
+from scipy.spatial.distance import squareform
+from scipy.cluster.hierarchy import fcluster, dendrogram, linkage
+from matplotlib import pyplot as plt
+
 class clusterByDistance:
+    """
+    Class to hold methods for generating clusters based on a distance matrix given as input.
+
+    The method uses the cluster hierarchical package from scipy and methods within.
+    """
 
     def __init__(self, distance_matrix, verbose=False):
+        """
+        At initialization the method calls the linkage method to generate an initialization
+        set of clusters.
+        """
 
-        self.distances = distance_matrix
+        self.distances = squareform(distance_matrix, checks=False)
         self.verbose = verbose
+
+        #Calculate clusters
+        self.distance_clusters = linkage(reduced_distances, method='average')
+
+        if verbose:
+            # Ignore diagonal values for max and min printing
+            mask = np.ones(D.shape, dtype=bool)
+            np.fill_diagonal(mask, 0)
+            max_value = D[mask].max()
+            min_value = D[mask].min()
+
+            print('Max pairwise distance: %.3f [diagonal values ignored]' % max_value)
+            print('Min pairwise distance: %.3f [diagonal values ignored]' % min_value)
